836c7ce53db79a2d6ce0a7697a2357d8d79ebeef
mac718 <mac718@gmail.com>
2018-04-17 23:03:02 -0400

    Prune routes

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,10 +1,10 @@
 module EventsHelper
-  def event_attendable(upcoming_events, current_event, current_user)
+  def event_attendable?(upcoming_events, current_event, current_user)
     upcoming_events.include?(current_event) && current_event.creator != current_user && 
       current_event.invitees.include?(current_user) && !current_event.attendees.include?(current_user)
   end
 
-  def invitation_requestable(current_event, current_user)
+  def invitation_requestable?(current_event, current_user)
     current_event.invitation_requests.where("requester_id = ?", current_user.id).empty? && 
       !current_user.attendances.include?(current_event) && current_user.id != current_event.creator_id
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'sessions#new'
-  resources :users
+  resources :users, only: [:new, :create, :show]
   get '/signup', to: 'users#new'
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   get '/signout', to: 'sessions#destroy'
-  resources :events do 
-    resources :event_attendances
-    resources :invitations
-    resources :invitation_requests
+  resources :events, only: [:new, :create, :show, :index] do 
+    resources :event_attendances, only: [:create]
+    resources :invitations , only: [:create]
+    resources :invitation_requests, only: [:create]
   end
 end
